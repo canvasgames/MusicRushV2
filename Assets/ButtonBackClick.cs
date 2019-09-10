1c0398146fadf466d9266998ecdf3f2c46c35b85
Eduardo <eduardo@canvasmobilegames.com>
2019-09-10 00:11:27 -0300

    Adding scripts for button click

---
+++ Assets/ButtonBackClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonBackClick : MonoBehaviour
+{
+    void Start()
+    {
+        Button btn = this.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        sound_controller.s.PlaySfxUIButtonBack();
+    }
+}
